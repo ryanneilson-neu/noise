ac000583ed3c1d6ea41c966ca51cd314a6986785
RNeilson <rneilson@vmhirBench102.mehealth.org>
2026-02-04 15:59:13 -0500

    Adding a testing script that will be used when changes are made to noise_inference.py

---
+++ noise_inference_testing.py
@@ -0,0 +1,9 @@
+from noise_inference import percent_ocl_area_per_well
+
+def testing():
+
+    print(percent_ocl_area_per_well(50,100))
+
+testing()
+
+# From the CLI, use python noise_inference_testing.py
